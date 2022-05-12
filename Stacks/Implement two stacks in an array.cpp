f977ed5256ecbd303b72e8a38e5a25b4ed70a6d3
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2022-05-12 15:03:08 +0530

    Create Implement two stacks in an array.cpp

---
+++ Stacks/Implement two stacks in an array.cpp
@@ -0,0 +1,50 @@
+https://practice.geeksforgeeks.org/problems/implement-two-stacks-in-an-array/1
+
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/*The structure of the class is
+class twoStacks
+{
+    int *arr;
+    int size;
+    int top1, top2;
+public:
+   twoStacks(int n=100){size = n; arr = new int[n]; top1 = -1; top2 = size;}
+ 
+   void push1(int x);
+   void push2(int x);
+   int pop1();
+   int pop2();
+};
+*/
+//Function to push an integer into the stack1.
+void twoStacks :: push1(int x)
+{
+    top1++;
+    arr[top1]=x;
+}
+   
+//Function to push an integer into the stack2.
+void twoStacks ::push2(int x)
+{
+    top2--;
+arr[top2]=x;
+}
+   
+//Function to remove an element from top of the stack1.
+int twoStacks ::pop1()
+{
+    if(top1 == -1)return -1;
+    return arr[top1--];
+}
+
+//Function to remove an element from top of the stack2.
+int twoStacks :: pop2()
+{
+    if(top2 == size)return -1;
+return arr[top2++];
+}
+
