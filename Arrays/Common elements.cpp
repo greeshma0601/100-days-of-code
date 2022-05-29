030325d73b67fadfe335b10c7258aae939fbd4c0
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2022-05-29 13:07:58 +0530

    Update Common elements.cpp

--- Arrays/Common elements.cpp
+++ Arrays/Common elements.cpp
@@ -5,38 +5,26 @@ Space complexity: O(n1 + n2 + n3)
 
 
 */     
-
-vector <int> commonElements (int a[], int b[], int c[], int n1, int n2, int n3)
-        {
-            //code here.
-            
-            vector<int>v;
-            unordered_set<int>s1,s2,s3;
-            
-            for(int i=0;i<n1;i++)
-            {
-                s1.insert(a[i]);
-            }
-            
-            for(int i=0;i<n2;i++)
-            {
-                 if(s1.find(b[i])!=s1.end())
-                s2.insert(b[i]);
-            }
-            
-            for(int i=0;i<n3;i++)
-            {
-                if(s2.find(c[i])!=s2.end())
-                {
-                    if(s3.find(c[i])==s3.end())
-                    {
-                        v.push_back(c[i]);
-                        s3.insert(c[i]);
-                    }
-                }
-            }
-            if(v.size() == 0){
-                v.push_back(-1);return v;
-            }
-            return v;
+void findCommon(int a[], int b[], int c[], int n1, int n2,
+                int n3)
+{
+    // three sets to maintain frequency of elements
+    unordered_set<int> uset, uset2, uset3;
+    for (int i = 0; i < n1; i++) {
+        uset.insert(a[i]);
+    }
+    for (int i = 0; i < n2; i++) {
+        uset2.insert(b[i]);
+    }
+    // checking if elements of 3rd array are present in
+    // first 2 sets
+    for (int i = 0; i < n3; i++) {
+        if (uset.find(c[i]) != uset.end()
+            && uset2.find(c[i]) != uset.end()) {
+            // using a 3rd set to prevent duplicates
+            if (uset3.find(c[i]) == uset3.end())
+                cout << c[i] << " ";
+            uset3.insert(c[i]);
         }
+    }
+}
