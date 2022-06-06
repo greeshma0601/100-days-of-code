0d7818fbb1d6d495620b220fc73d6090475a9593
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2022-06-06 18:50:57 +0530

    Create Bitmanipulation.cpp

---
+++ Bitmanipulation.cpp
@@ -0,0 +1,30 @@
+/*
+https://leetcode.com/problems/number-of-1-bits/
+https://www.youtube.com/watch?v=5Km3utixwZs
+*/
+//two methods
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res =0;
+        while(n)
+        {
+            n = n & (n-1);
+            res++;
+        }
+        return res;
+    }
+};
+/*
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res =0;
+        while(n)
+        {
+            res += n%2;
+            n =  n>>1;
+        }
+        return res;
+    }
+};*/
