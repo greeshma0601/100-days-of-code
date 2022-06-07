69b190a67c542c34081019e01f1f6cd4c7de6dee
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2022-06-07 13:37:22 +0530

    Update Counting bits.cpp

--- Bitmanipulation/Counting bits.cpp
+++ Bitmanipulation/Counting bits.cpp
@@ -3,6 +3,27 @@ https://leetcode.com/problems/counting-bits/
 https://www.youtube.com/watch?v=RyBM56RIWrM
 */
 
+1.Dynamic Programming 
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>dp(n+1);
+        dp[0]=0;
+        int offset = 1;
+        
+        for(int i=1;i<=n;i++)
+        {    if(offset*2 == i)
+                offset = i;
+            dp[i] = 1+dp[i-offset];
+        }
+        return dp;
+    }
+};
+
+
+2.Counting bits using and operation
+
 class Solution {
 public:
     vector<int> countBits(int n) {
