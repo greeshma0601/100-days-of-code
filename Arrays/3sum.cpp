7a57c0b1328cd738d5526ce87b9b63c7b1c45dfd
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2022-06-11 17:12:58 +0530

    Update 3sum.cpp

--- Arrays/3sum.cpp
+++ Arrays/3sum.cpp
@@ -2,6 +2,16 @@
 https://leetcode.com/problems/3sum/
 https://www.youtube.com/watch?v=jzZsG8n2R9A
 https://neetcode.io/
+https://www.geeksforgeeks.org/find-a-triplet-that-sum-to-a-given-value/
+
+Algorithm : 
+Sort the given array.
+Loop over the array and fix the first element of the possible triplet, arr[i].
+Then fix two pointers, one at i + 1 and the other at n – 1. And look at the sum, 
+If the sum is smaller than the required sum, increment the first pointer.
+Else, If the sum is bigger, Decrease the end pointer to reduce the sum.
+Else, if the sum of elements at two-pointer is equal to given sum then print the triplet and break.
+
 */
 
 class Solution {
