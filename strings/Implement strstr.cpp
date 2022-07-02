f309de1f8ef5803c7bc6cebb04d340ba992f2e3c
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2022-07-02 12:32:59 +0530

    Update Implement strstr.cpp

--- strings/Implement strstr.cpp
+++ strings/Implement strstr.cpp
@@ -10,3 +10,30 @@ public:
         
     }
 };
+
+Solution 2
+    
+    class Solution {
+public:
+    int strStr(string s2, string s1) {
+             int M = s1.length();
+    int N = s2.length();
+ 
+    /* A loop to slide pat[] one by one */
+    for (int i = 0; i <= N - M; i++) {
+        int j;
+ 
+        /* For current index i, check for
+ pattern match */
+        for (j = 0; j < M; j++)
+            if (s2[i + j] != s1[j])
+                break;
+ 
+        if (j == M)
+            return i;
+    }
+ 
+    return -1;
+        
+    }
+};
