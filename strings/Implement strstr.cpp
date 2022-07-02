398c500c1127dfa7e55eee61f972f1f0d5b9d5ed
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2022-07-02 12:15:35 +0530

    Create Implement strstr.cpp

---
+++ strings/Implement strstr.cpp
@@ -0,0 +1,12 @@
+/*
+https://leetcode.com/problems/implement-strstr/
+https://www.geeksforgeeks.org/check-string-substring-another/
+*/
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+            return haystack.find(needle);
+        
+    }
+};
