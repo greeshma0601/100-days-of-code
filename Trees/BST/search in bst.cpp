f0b7363b9b558618a51fae6df62d0773d0a1d354
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2022-07-03 13:24:51 +0530

    Create search in bst.cpp

---
+++ Trees/BST/search in bst.cpp
@@ -0,0 +1,28 @@
+/*
+https://leetcode.com/problems/search-in-a-binary-search-tree/submissions/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root && root->val!=val)
+        {
+            if(root->val<val)
+                root = root->right;
+            else
+                root = root->left;
+        }
+        return root;
+    }
+};
